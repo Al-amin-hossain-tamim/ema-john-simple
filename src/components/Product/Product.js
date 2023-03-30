90797dcd4c917c3c8fffa887af3273a9c743d751
Alamin-hossain-tamim <engrsmalaminhossain@gmail.com>
2023-03-30 23:57:45 +0600

    ui complete

---
+++ src/components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {name,img,seller,price,ratings} = props.product;
+    return (
+        <div className='product'>
+
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>price:${price}</p>
+            <p>Manufacturer:{seller}</p>
+            <p>Rating:{ratings} start</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+
+        </div>
+    );
+};
+
+export default Product;
