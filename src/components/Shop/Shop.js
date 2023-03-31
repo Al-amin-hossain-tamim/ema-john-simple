f350e8ae88f6f464a1dab35232da897a8c6755bc
Alamin-hossain-tamim <engrsmalaminhossain@gmail.com>
2023-03-31 09:37:54 +0600

    button complete

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import './Shop.css'
 
 const Shop = () => {
 const [products,setProducts] = useState([]);
+const [cart,setCart] = useState([]);
 
 useEffect( () =>{
     fetch('products.json')
@@ -11,19 +12,26 @@ useEffect( () =>{
     .then(data=> setProducts(data))
 },[])
 
+const handleAddToCart = (product)=>{
+    const newCart = [...cart,product];
+    setCart(newCart);
+}
+
     return (
         <div className='shop-container'>
             <div className="products-container">
             {
                 products.map(product =><Product
-                
+
                 key ={product.id}
                 product = {product}
+                handleAddToCart = {handleAddToCart}
                 ></Product>)
             }
             </div>
             <div className="card-summary">
                 <h4>Order summary</h4>
+                <p>Selected items:{cart.length}</p>
             </div>
         </div>
     );
